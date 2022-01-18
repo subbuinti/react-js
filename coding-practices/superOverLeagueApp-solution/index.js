673854dafac533fa013550fd941e7328e0d8180e
subbuinti <subbuinti1@gmail.com>
2022-01-18 07:53:01 +0000

    super over league app

--- coding-practices/superOverLeagueApp-solution/index.js
+++ coding-practices/superOverLeagueApp-solution/index.js
@@ -1,5 +1,4 @@
 const element = (
-  //   Write your code here.
   <div className="super-over-league-container">
     <h1 className="heading">Super Over League</h1>
     <div className="teams-container">
@@ -13,6 +12,6 @@ const element = (
       />
     </div>
   </div>
-);
+)
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'))
