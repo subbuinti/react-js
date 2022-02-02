f47452cc3bb90858c0fd046c3de58343065eca77
subbuinti <subbuinti1@gmail.com>
2022-02-02 03:23:28 +0000

    introduction react

---
+++ sessions/Introduction-to-React JS/index.js
@@ -0,0 +1,5 @@
+const rootElement = document.getElementById("root");
+const element = document.createElement("h1");
+element.textContent = "Hello World!";
+element.classList.add("greeting");
+rootElement.appendChild(element);
