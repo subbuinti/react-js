0eefaf86bc9d9434edf4bc84159edf5ab9d27e06
subbuinti <subbuinti1@gmail.com>
2022-02-02 04:26:01 +0000

    introduction react

--- sessions/Introduction-to-React JS/index.js
+++ sessions/Introduction-to-React JS/index.js
@@ -1,4 +1,4 @@
 const elementProps = { className: "greeting", children: "Hello world!" };
-const elementType = "h1";
+const elementType = "h2";
 const element = React.createElement(elementType, elementProps);
 ReactDOM.render(element, document.getElementById("root"));
