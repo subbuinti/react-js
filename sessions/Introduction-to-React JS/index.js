c772514efb23b516b6990abb9ac980a89ec4a694
subbuinti <subbuinti1@gmail.com>
2022-02-02 07:40:17 +0000

    introduction react

--- sessions/Introduction-to-React JS/index.js
+++ sessions/Introduction-to-React JS/index.js
@@ -1,4 +1,8 @@
-const name = "Rahul";
-const className = "greeting";
-const element = <h1 className={className}>Hello {name}!</h1>;
+const fullName = (user) => user.firstName + " " + user.lastName;
+const user = { firstName: "Rahul", lastName: "attluri" };
+const element = (
+  <div>
+    <h1 className="greeting">Hello, {fullName(user)}</h1>;<p>welcome</p>;
+  </div>
+);
 ReactDOM.render(element, document.getElementById("root"));
