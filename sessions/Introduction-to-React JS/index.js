f36dca0609c41e005f0511c8da20eca4ca14dc58
subbuinti <subbuinti1@gmail.com>
2022-02-08 05:21:26 +0000

    changed

--- sessions/Introduction-to-React JS/index.js
+++
@@ -1,9 +0,0 @@
-const fullName = (user) => user.firstName + " " + user.lastName;
-const user = { firstName: "Rahul", lastName: "attluri" };
-const element = (
-  <div>
-    <h1 className="greeting">Hello, {fullName(user)}</h1>
-    <p>welcome</p>
-  </div>
-);
-ReactDOM.render(element, document.getElementById("root"));
